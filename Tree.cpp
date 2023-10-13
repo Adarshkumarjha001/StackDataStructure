37ab52e081da4e34ee8e28415d0fa8ac14db140b
Adarsh Kumar Jha <117103488+Adarshkumarjha001@users.noreply.github.com>
2023-10-13 17:49:09 +0530

    13-10-2023
    Problem related to tree.

---
+++ Tree.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+	public:
+		int data;
+		Node* left;
+		Node* right;
+		Node(int val){
+			data=val;
+			left=NULL;
+			right=NULL;
+			
+		}
+};
+void preorder(Node* root){
+	if(root==NULL) return;
+	cout<<root->data<<" ";
+	preorder(root->left);
+	preorder(root->right);
+		
+	
+}
+void inorder(Node* root){
+	if(root==NULL) return;
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+void postorder(Node* root){
+	if(root==NULL){
+		return;
+	}
+	postorder(root->right);
+	postorder(root->left);
+	cout<<root->data<<" ";
+}
+int main(){
+Node* root=new Node(1); 
+root->left=new Node(2);
+root->right=new Node(3);
+root->left->left=new Node(4);
+root->left->right=new Node(5);
+root->right->left=new Node(6);
+root->right->right=new Node(7);
+cout<<"Preorder Arrangement is: ";
+preorder(root);
+cout<<endl;
+cout<<"Inorder Arrangement is: ";
+inorder(root);
+cout<<endl;
+cout<<"postorder Arrangement is: ";
+postorder(root);
+ 
+
+}
